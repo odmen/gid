693905c11f3bfab9dcabc518e21878839de53e0c
odmen <odmen666@gmail.com>
2014-05-14 00:44:22 +0400

    Добавил комментарии

--- gid.py
+++ gid.py
@@ -11,12 +11,17 @@ import shutil
 from functools import partial
 
 for arg in sys.argv:
+# перебираем аргументы, переданные скрипту
   currindex = sys.argv.index(arg)
+# запоминаем индекс текущего выбранного аргумента
   if arg == "-p":
+# если текущий элемент - "-p"
     work_dir = sys.argv[currindex+1]
+# то запоминаем текст следующего (по индексу) элемента как "рабочий каталог"
 
-# считает md5 сумму файла
+# эта функция считает md5 сумму файла
 # на вход принимает полный путь до файла
+# код фунции сопирован из интернета, как работает пока не смотрел
 def md5sum(filename):
     with open(filename, mode='rb') as f:
         d = hashlib.md5()
@@ -25,24 +30,40 @@ def md5sum(filename):
     return d.hexdigest()
 
 hash_names = {}
+# будет хранить соответствия хеша файла перечню имен одинаковых файлов
 img_regex = re.compile("\.(jpg|jpeg|png)$")
+# регулярка выборки файлов определенного разрешения
 in_csv = work_dir+"in.csv"
+# составляем из переданного скрипту рабочего каталога путь до исходного файла CSV
 out_csv = work_dir+"out.csv"
+# аналогично для файла результата
 
 os.chdir(work_dir)
+# переходим в рабочий каталог
 for filename in glob.glob("*.*"):
+# перебираем имена файлов в этом каталоге
   if img_regex.search(filename):
+# если текущий выбранный файл нам подходит по регулярке
     filemd5 = md5sum(work_dir+filename)
-    # print(filemd5+" "+filename)
+# передаем путь до файла в функцию вычисления md5
     if filemd5 in hash_names:
+# если такой хеш уже есть в словаре
       hash_names[filemd5].append(filename)
+# добавляем имя файла в список к соотв. хешу
     else:
+# если нет, то обновляем словарь, добавив имя текущего выбранного файла
       hash_names.update({filemd5:[filename]})
 
+
 with open(in_csv, "r") as infile, open(out_csv, "w") as outfile:
+# открываем оба CSV. один для чтения, второй для записи
     r = csv.DictReader(infile, delimiter=';')
+# поможет удобно читать csv файл. Задаем разделитель полей
     w = csv.DictWriter(outfile, r.fieldnames, delimiter=';')
+# поможет удобно записать csv файл.
+# Задаем разделитель полей и заголовки
     w.writeheader()
+# пишем заголовки таблицы
     for row in r:
 # обходим каждую строку
       for fhash in hash_names:
